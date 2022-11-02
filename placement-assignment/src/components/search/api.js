83ec4e8fd7aad1aa9b0ccc154762dc013c3ee820
prarathod <itsmeprajwal2@gmail.com>
2022-11-02 14:39:01 +0530

    added search functionality

---
+++ placement-assignment/src/components/search/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '46ab861802msh0217e155add48c4p108fc9jsn0393b33d50de',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const OneCallAPI = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=39e8760b88d9a1a138a959410e486511";
---
+++ placement-assignment/src/components/search/search.js
@@ -0,0 +1,58 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+import React, { useState } from "react";
+import { GEO_API_URL, geoApiOptions } from "./api"
+import "./search.css"
+const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null);
+    
+    const Fetchlocation = async()=>{
+        let url = "https://ipinfo.io/json?token=41f27fa180abe4";
+        let response = await fetch(url);
+        let data = await response.json(); 
+        await navigator.geolocation.getCurrentPosition((position)=>{     
+        setSearch({
+                value: `${position.coords.latitude} ${position.coords.longitude}`,
+                label: `${data.city}, ${data.country}`,
+            });
+        });
+    }
+    React.useEffect(()=>{    
+        Fetchlocation();
+    },[]);
+    const loadOptions = (inputValue) => {
+        
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=10000&namePrefix=${inputValue}`, 
+            geoApiOptions
+            )
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
+    }
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+    return (
+        <div className="search">
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+        </div>
+    )
+}
+export default Search;
